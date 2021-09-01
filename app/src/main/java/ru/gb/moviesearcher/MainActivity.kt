8d26bd4be1e633a60322715b298ad9094ce12519
Egor M <merkushev.eg@gmail.com>
2021-09-01 17:54:38 +0200

    Lesson2_problem with  NullPointerEx

--- app/src/main/java/ru/gb/moviesearcher/MainActivity.kt
+++ app/src/main/java/ru/gb/moviesearcher/MainActivity.kt
@@ -2,17 +2,67 @@ package ru.gb.moviesearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
 import ru.gb.moviesearcher.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.main_content)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        initToolbar();
+
+    }
+
+    private fun initToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+        val search: MenuItem = menu!!.findItem(R.id.action_search)//разобраться с !! и ?
+
+        val searchText: SearchView =  search.actionView as SearchView
+        searchText.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show();
+                return true
+
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+
+        return true
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        var id: Int = item.itemId
+
+        when (id){
+            R.id.action_settings -> {
+                Toast.makeText(this@MainActivity,"Settings", Toast.LENGTH_SHORT)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/ru/gb/moviesearcher/ui/main/MainFragment.kt
+++ app/src/main/java/ru/gb/moviesearcher/ui/main/MainFragment.kt
@@ -1,11 +1,12 @@
 package ru.gb.moviesearcher.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import ru.gb.moviesearcher.R
 
 class MainFragment : Fragment() {
@@ -20,13 +21,21 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.main_fragment, container, false)
+
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        super.onViewCreated(view, savedInstanceState)
     }
+
+
 
 }
