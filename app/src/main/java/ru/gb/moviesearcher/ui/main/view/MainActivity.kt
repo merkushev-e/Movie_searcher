c35c02a90d774587c9a60f00fc5c8a7a536a0dc2
Egor M <merkushev.eg@gmail.com>
2021-10-18 15:21:42 +0200

    lesson12

--- app/src/main/java/ru/gb/moviesearcher/ui/main/view/MainActivity.kt
+++ app/src/main/java/ru/gb/moviesearcher/ui/main/view/MainActivity.kt
@@ -32,7 +32,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
